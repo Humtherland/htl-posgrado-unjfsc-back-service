62627e602089fc367aaec5fb65360420c0b9e020
samuel14luis <luismendoza.samuel@gmail.com>
2023-03-28 22:33:23 -0500

    feat: enable config module to load configs from .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${ process.env.PORT }`);
 }
+
 bootstrap();
