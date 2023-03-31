201be9a3534c1edb49e9e690ef8b967119a6fdeb
samuel14luis <luismendoza.samuel@gmail.com>
2023-03-31 00:38:46 -0500

    feat: configuration for jwt generation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './entities/user.entity';
 
 @Module({
@@ -11,7 +13,20 @@ import { User } from './entities/user.entity';
   imports: [
     ConfigModule,
     TypeOrmModule.forFeature([ User ]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: configService.get('JWT_DURATION')
+          }
+        }
+      }
+    })
   ],
-  exports: [ TypeOrmModule ]
+  exports: [ TypeOrmModule, PassportModule, JwtModule ]
 })
 export class AuthModule {}
