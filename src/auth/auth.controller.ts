eb26f5871dde7601ee5fb2df7cf080cf78867020
samuel14luis <luismendoza.samuel@gmail.com>
2023-04-01 18:59:16 -0500

    feat: enable role protection by role

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,9 +2,11 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { Req, UseGuards } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { GetUser, RawHeaders } from './decorators';
+import { GetUser, RawHeaders, RoleProtected } from './decorators';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -32,6 +34,19 @@ export class AuthController {
     return {
       ok: true,
       message: 'Hola Mundo Private',
+    }
+  }
+
+  @Get('private2')
+  @RoleProtected( ValidRoles.superUser, ValidRoles.admin )
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+
+    return {
+      ok: true,
+      user
     }
   }
   
